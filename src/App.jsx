b9e05cd7b3ef95f038d8945666d323e6fb30e7c3
Alin <alinalin03030@gmail.com>
2025-05-04 23:31:04 +0000

    Exercise 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,41 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <ul>
-        <li>Great for front-end development</li>
-        <li>Great for back-end development</li>
-        <li>Great for full-stack development</li>
-        <li>Great for mobile development</li>
-        <li>Great for desktop development</li>
-      </ul>
+      <header>
+        <img src="../public/images/React.webp" alt="React Logo" width="100" />
+      </header>
+
+      <main>
+        <h1>This is a cool ass page</h1>
+
+        <ul>
+          <li>Great for front-end development</li>
+          <li>Great for back-end development</li>
+          <li>Great for full-stack development</li>
+          <li>Great for mobile development</li>
+          <li>Great for desktop development</li>
+        </ul>
+      </main>
+      <footer>© 20xx Alexandru development. All rights reserved.</footer>
     </div>
   )
 }
 
 export default App
+
+
+/**
+Challenge: 
+
+Part 2: 
+- Add a `<header>` element with an `<img />` element with the image of the 
+  React logo inside (src="react-logo.png") and make sure to set the 
+  width to something more manageable so it doesn't take up the whole screen.
+  Also, as always, you should include some alt text on the image.
+- Add an `<h1>` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list, then wrap
+  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+  structure flowing well.
+- Add a `<footer>` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
